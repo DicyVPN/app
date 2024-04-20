8fe076f9ee627bcef5941d06710fd2a5fd4b5578
Lorenzo Lapucci <halexercraft@gmail.com>
2024-04-20 18:17:29 +0200

    Start building the Home page: show server countries list

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,9 @@
+import 'dart:developer';
+
+import 'package:dicyvpn/ui/api/api.dart';
+import 'package:dicyvpn/ui/api/dto.dart';
 import 'package:dicyvpn/ui/components/button.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -9,35 +14,108 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _loading = true;
+  Map<String, List<Server>> _primaryServers = {};
+  Map<String, List<Server>> _secondaryServers = {};
 
   @override
   Widget build(BuildContext context) {
+    var primaryServersKeys = _primaryServers.keys.toList();
+    var secondaryServersKeys = _secondaryServers.keys.toList();
+
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            const Text('home page'),
-            Button(
-              onPressed: () {
-                Navigator.pushNamed(context, '/logout');
-              },
-              theme: CustomButtonTheme.dark,
-              color: CustomButtonColor.red,
-              size: CustomButtonSize.big,
-              child: const Text('Logout'),
+        child: CustomScrollView(
+          slivers: [
+            const SliverPadding(padding: EdgeInsets.only(bottom: 16)),
+            SliverPadding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              sliver: SliverToBoxAdapter(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Button(
+                      onPressed: _fetchServers,
+                      theme: CustomButtonTheme.dark,
+                      color: CustomButtonColor.blue,
+                      size: CustomButtonSize.big,
+                      child: const Text('Load servers'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(12, 12, 12, 4),
+                      child: Text(tr('recommendedServers')),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            Button(
-              onPressed: () {
-                Navigator.pushNamed(context, '/login', arguments: 'Your session has expired');
-              },
-              theme: CustomButtonTheme.dark,
-              color: CustomButtonColor.green,
-              size: CustomButtonSize.big,
-              child: const Text('TEST BUTTON'),
+            const SliverPadding(padding: EdgeInsets.symmetric(vertical: 4)),
+            SliverPadding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              sliver: SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    return Container(
+                      height: 50,
+                      color: Colors.amber,
+                      child: Center(child: Text('Entry ${primaryServersKeys[index]}')),
+                    );
+                  },
+                  childCount: primaryServersKeys.length,
+                ),
+              ),
             ),
+            const SliverPadding(padding: EdgeInsets.symmetric(vertical: 4)),
+            SliverPadding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              sliver: SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                  child: Text(tr('otherServers')),
+                ),
+              ),
+            ),
+            const SliverPadding(padding: EdgeInsets.symmetric(vertical: 4)),
+            SliverPadding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              sliver: SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    return Container(
+                      height: 50,
+                      color: Colors.amber,
+                      child: Center(child: Text('Secondary: ${secondaryServersKeys[index]}')),
+                    );
+                  },
+                  childCount: secondaryServersKeys.length,
+                ),
+              ),
+            ),
+            const SliverPadding(padding: EdgeInsets.only(top: 16))
           ],
         ),
       ),
     );
+  }
+
+  void _fetchServers() async {
+    setState(() {
+      _loading = true;
+    });
+
+    var api = await API.get();
+    try {
+      var serverList = await api.getServersList();
+      setState(() {
+        _loading = false;
+        _primaryServers = serverList.primary;
+        _secondaryServers = serverList.secondary;
+      });
+    } catch (e) {
+      log('Failed to get servers list', name: 'DicyVPN/Home', error: e);
+      setState(() {
+        _loading = false;
+      });
+    }
   }
 }
