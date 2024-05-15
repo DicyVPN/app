f372d2e3082b205b45e4ff8f2d7be4cbdc1a5f12
Lorenzo Lapucci <halexercraft@gmail.com>
2024-05-15 18:18:29 +0200

    Started implementing method channels for iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,44 @@
 import UIKit
 import Flutter
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    private var wgMethodChannel = "wireguard_native.dicyvpn.com/method"
+    private var wgEventChannel = "wireguard_native.dicyvpn.com/event"
+    
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        FlutterMethodChannel(name: wgMethodChannel, binaryMessenger: controller.binaryMessenger)
+            .setMethodCallHandler({
+                (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+                switch call.method {
+                case "requestPermission":
+                    //self.requestPermission(result)
+                    break
+                case "start":
+                    let config: String? = (call.arguments as? [String: String])?["config"]
+                    if config != nil {
+                        //self.start(config, result)
+                    } else {
+                        result(FlutterError(code: "missing_argument", message: "Missing argument 'config'", details: nil))
+                    }
+                    break
+                case "stop":
+                    //self.stop(result)
+                    break
+                case "getStatus":
+                    //result(DicyVPN.getTunnel().getStatus().value)
+                    break
+                default:
+                    result(FlutterError(code: "not_implemented", message: "Method '" + call.method + "' is not implemented", details: nil))
+                }
+            })
+        
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
